81f7fa382fa4e9f103b0620292ba337d921473f0
zhipenglin <sunandmoon001@163.com>
2024-06-06 17:50:13 +0800

    '初始化项目'

---
+++ README.md
@@ -0,0 +1,11 @@
+# email-template
+
+提供用于发送email的内容模板
+
+[更多文档](https://www.kne-union.top/#/components)
+
+开始：
+
+```shell
+npm run start
+```
---
+++ src/preset.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { preset as fetchPreset } from '@kne/react-fetch';
+import { Spin, Empty, message } from 'antd';
+import axios from 'axios';
+import { preset as remoteLoaderPreset } from '@kne/remote-loader';
+import omit from 'lodash/omit';
+
+window.PUBLIC_URL = process.env.PUBLIC_URL;
+
+const registry = {
+  url: 'https://uc.fatalent.cn',
+  tpl: '{{url}}/packages/@kne-components/{{remote}}/{{version}}/build'
+};
+
+const componentsCoreRemote = {
+  ...registry,
+  remote: 'components-core',
+  defaultVersion: '0.1.57'
+};
+
+remoteLoaderPreset({
+  remotes: {
+    default: componentsCoreRemote,
+    'components-core': componentsCoreRemote,
+    'components-iconfont': {
+      ...registry,
+      remote: 'components-iconfont',
+      defaultVersion: '0.1.3'
+    },
+    'email-template':
+      process.env.NODE_ENV === 'development'
+        ? {
+            remote: 'email-template',
+            url: '/',
+            tpl: '{{url}}'
+          }
+        : {
+            ...registry,
+            remote: 'email-template',
+            defaultVersion: process.env.DEFAULT_VERSION
+          }
+  }
+});
+
+export const ajax = (() => {
+  const instance = axios.create({
+    validateStatus: function () {
+      return true;
+    }
+  });
+
+  return params => {
+    if (params.hasOwnProperty('loader') && typeof params.loader === 'function') {
+      return Promise.resolve(params.loader(omit(params, ['loader'])))
+        .then(data => ({
+          data: {
+            code: 0,
+            data
+          }
+        }))
+        .catch(err => {
+          message.error(err.message || '请求发生错误');
+          return { data: { code: 500, msg: err.message } };
+        });
+    }
+    return instance(params);
+  };
+})();
+
+fetchPreset({
+  ajax,
+  loading: <Spin delay={500} style={{ position: 'absolute', left: '50%', padding: '10px', transform: 'translateX(-50%)' }} />,
+  error: null,
+  empty: <Empty />,
+  transformResponse: response => {
+    const { data } = response;
+    response.data = {
+      code: data.code === 0 ? 200 : data.code,
+      msg: data.msg,
+      results: data.data
+    };
+    return response;
+  }
+});
